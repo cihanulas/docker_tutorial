cfb1cf1274fee869f9e62577e6e84cb61eccb473
Cihan Ulas <culas@isravision.com>
2020-10-16 14:02:07 +0300

    do some change.

--- production_workflow/frontend/src/App.js
+++ production_workflow/frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Make some changes <code>src/App.js</code> and save to reload.
+          Just some changes <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
